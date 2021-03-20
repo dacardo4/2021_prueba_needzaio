7695c2c229a6477a45fea9a79cd87bda0b85c843
prueba <42043332+dacardo4@users.noreply.github.com>
2021-03-19 19:05:52 -0500

    diseño footer

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'footer',
+  selector: 'appFooter',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })

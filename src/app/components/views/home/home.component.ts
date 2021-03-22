4f5896d4ab7876359fd4a1c722e9778365a0bd95
prueba <42043332+dacardo4@users.noreply.github.com>
2021-03-21 21:33:27 -0500

    focus section since navbar

--- src/app/components/views/home/home.component.ts
+++ src/app/components/views/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+  
+  ngAfterViewInit() {
+    console.log("AFTTTTTEEEEEEEERRRRRRRR");
+    this._activatedRoute.url.subscribe(url =>{
+      if (this._activatedRoute.snapshot.params['id'] !== undefined) {
+        switch (this._activatedRoute.snapshot.params['id']) {
+          case "location":
+            this.focusOnSection("1");
+            break;
+          case "blog":
+            this.focusOnSection("2");
+            break;
+          case "services":
+            this.focusOnSection("3");
+            break;
+          case "aboutUs":
+            this.focusOnSection("4");
+            break;
+          case "franchiseWithUs":
+            this.focusOnSection("5");
+            break;
+        }
+      }
+    });
+  }
+
+  focusOnSection(number: string){
+    window.location.hash = `#section00${number}`;
   }
 
 }
